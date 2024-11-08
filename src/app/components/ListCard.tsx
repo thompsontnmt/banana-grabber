e27b681e846b573e24347afb61a9b5d79676f52b
maryconnelly <maryconnelly14@gmail.com>
2024-11-07 21:56:26 -0600

    Made the ListCard

--- src/app/components/ListCard.tsx
+++ src/app/components/ListCard.tsx
@@ -6,35 +6,17 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
 export default function ListCard() {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+    <Card sx={{ 
+        width: 1208 ,
+        height: 642 ,
+        bgcolor: '#D9D9D9' ,
+        borderRadius: 10 ,
+    }}>
+      
+      
     </Card>
   );
 }
