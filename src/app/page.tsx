ae79f366a1e45fd24cf97a6369fc27397c679b6b
Mary Connelly <maryconnellye@gmail.com>
2025-02-24 10:33:53 -0600

    fixed formatting (should I have been in a separate branch?)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,39 @@ const ToDoList = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column', 
+        display: 'grid',
+        gridTemplate: '1fr 4fr / 1fr',
+        gridAutoFlow: 'column', 
         height: '100vh', // This sets the height to 100% of the viewport height
         overflow: 'hidden',
+
       }}
     >
       <Button sx={{
-        width: 50,
-        height: 200,
+        gridArea: '1 / 1 / 2 / 2',
+        justifySelf: 'start',
+        alignSelf: 'center',
+        height: 70,
+        width: 250,
         bgcolor: 'blue',
-        borderRadius: 10,
-      }}>
-        Test
+        color: 'white',
+        borderRadius: 2,
+        fontFamily: 'tahoma',
+        fontSize: 34,
+        marginLeft: 3,
+        textTransform: 'none',
+        
+      }}> +Add To Do
       </Button>
+
       <Card sx={{
-        width: 800,
+        width: 1000,
         height: 500,
         bgcolor: '#D9D9D9',
-        borderRadius: 10,
+        borderRadius: 2,
+        justifySelf: 'center',
       }} />
+
     </Box>
 
   );
